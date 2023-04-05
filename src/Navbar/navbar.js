c9eba4a8c11e824a24278b91b103b3d5fb565aa7
Sarvesh Mishra <93536099+Sarvesh0916@users.noreply.github.com>
2023-04-05 11:12:05 +0530

    update

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -2,15 +2,23 @@ import React from 'react'
 import './navbar.css';
 
 export default function Navbar() {
+    const btnClick =(e) =>{
+        document.getElementById("p").innerHTML="You're viewing "+ e.target.innerText +" section.";
+    }
+    
+
     return (
-        <header className="header">
-            <h1 className="logo">My Nav</h1>
+        <>        <header className="header">
+            <h1 className="logo">NavBar</h1>
             <ul className="nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#Home" onClick={btnClick}>Home</a></li>
+                <li><a href="#About" onClick={btnClick}>else</a></li>
+                <li><a href="#Projects" onClick={btnClick}>Projects</a></li>
+                <li><a href="#Connect" onClick={btnClick}>Contact</a></li>
             </ul>
         </header>
+        <div id='div'><p id="p"></p></div>
+        </>
+
     )
 }
